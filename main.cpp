8d140b226351dd6fcef0e4f484ca9000e855baa6
Daniel Kovachev <danielkovachev@daniels-mbp.home>
2024-05-29 19:19:35 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+class Person {
+    public:
+        string name;
+
+//        Constructor
+        Person(string n) {
+            name = n;
+        }
+
+        void sayName() {
+//            printf("%s", name.c_str());
+            cout << name;
+        }
+} pers_a("Danny");
+
+
+int main() {
+    puts(pers_a.name.c_str());
+    pers_a.sayName();
+
+    //Hello test
+}
